f30c7cd5e22c7f9a633d22d95d15f9bb7d7aeabc
Joshua Hall <joshdhal@amazon.com>
2023-02-06 20:09:30 +0000

    Add correct setup and integ-templates for pipeline

--- Snowflake-Role-Grant/setup.sh
+++ Snowflake-Role-Grant/setup.sh
@@ -2,5 +2,5 @@
 #
 # This script runs in the buildspec before testing
 
-cat example_inputs/inputs_1_create.json | sed "s/USER_NAME/${USER_NAME}/g" > inputs/inputs_1_create.json
-cat test/integ-template.yml | sed "s/USER_NAME/${USER_NAME}/g" > test/integ.yml
+cat example_inputs/inputs_1_create.json | sed "s/USER_NAME/${USER_NAME}/g" | sed "s/ROLE_NAME/${ROLE_NAME}/g" > inputs/inputs_1_create.json
+cat test/integ-template.yml | sed "s/USER_NAME/${USER_NAME}/g" | sed "s/ROLE_NAME/${ROLE_NAME}/g" > test/integ.yml
